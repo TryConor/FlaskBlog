034c4f319adcd54a3d96fade4b753372033d0b97
TryConor <cjmw5t@umsystem.edu>
2024-11-01 19:06:14 -0500

    final push
    final

--- flask_blog_class/app.py
+++ flask_blog_class/app.py
@@ -88,4 +88,28 @@ def edit(id):
     # If GET then display the page
     return render_template('edit.html', post=post)
 
+#create a route to delete a post
+#delete will only be processed with a post method 
+#the post id is the url parameter
+@app.route('/int:id>/delete', methods=('POST',))
+def delete(id):
+    #get the post
+    post = get_post(id)
+
+    #connect to the database
+    conn = get_db_connection()
+
+    #execute a delete query 
+    conn.execute('Delete from posts WHERE id = ?', (id,))
+
+    #commit and close the connection 
+    conn.commit()
+    conn.close()
+
+    #flash a success message
+    flash('"{}" was succesfully deleted!'.format(post['title']))
+
+    #redirect from the homepage 
+    return redirect(url_for('index'))
+
 app.run(port=5008)
